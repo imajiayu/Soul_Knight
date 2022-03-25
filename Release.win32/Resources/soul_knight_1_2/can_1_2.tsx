e2da38a2db8b18e794639de19ad5647eeec167e4
Majiayu <57378499+imajiayu@users.noreply.github.com>
2022-03-25 16:58:36 +0800

    Add files via upload

---
+++ Release.win32/Resources/soul_knight_1_2/can_1_2.tsx
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="can_1_2" tilewidth="48" tileheight="48" tilecount="4" columns="4">
+ <image source="../can.png" width="192" height="48"/>
+ <tile id="0">
+  <properties>
+   <property name="Hot" type="bool" value="true"/>
+   <property name="Move" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="Poison" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="Cold" type="bool" value="true"/>
+   <property name="Eat" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="Cross" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
