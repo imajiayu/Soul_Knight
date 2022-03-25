e00301a388af02ed58eae2abdce8ef058370776c
Majiayu <57378499+imajiayu@users.noreply.github.com>
2022-03-25 17:07:22 +0800

    Add files via upload

---
+++ Release.win32/Resources/meta_tiles_1_1.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="meta_tiles" tilewidth="48" tileheight="48" tilecount="5" columns="5">
+ <image source="meta_tiles.png" width="280" height="70"/>
+ <tile id="0">
+  <properties>
+   <property name="unmovale" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="canEat" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
