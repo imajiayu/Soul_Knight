0872f4dc6cfd9408bb0949311a0c9aecfaa23310
Majiayu <57378499+imajiayu@users.noreply.github.com>
2022-03-25 17:08:37 +0800

    Add files via upload

---
+++ Release.win32/Resources/通行度.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="通行度" tilewidth="48" tileheight="48" tilecount="2660" columns="76">
+ <image source="通行度.png" width="3688" height="1708"/>
+ <tile id="0">
+  <properties>
+   <property name="Collidable" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
